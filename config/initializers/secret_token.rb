b8c33e513f9ff7b21f8d9a5eafdc63b700eb47f5
LeightonMa <imxl14@gmail.com>
2013-12-21 00:06:09 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JellydemoApp::Application.config.secret_key_base = 'a7bbdce237e0e97a6ae1a29030e6d15ccec74a7bd23cf44507e745cfc46577a0232ab9b1b88223e640b9297b337da6af4d8e97a201df086079f77168f11d1b91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JellydemoApp::Application.config.session_store :cookie_store, key: '_jellydemo_app_session'
